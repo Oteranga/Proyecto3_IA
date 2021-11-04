bad40001f87eb37196187d11504b31b9c9a4f351
Alejandro Otero <alejo.janoas@gmail.com>
2021-11-04 09:18:14 -0500

    dbscan algorithm almost implemented

--- aux.py
+++ aux.py
@@ -1,4 +1,6 @@
 import math
+from sklearn.neighbors import KDTree
+import numpy as np
 
 def distance(centroid,current_row):
     dist = 0
@@ -10,5 +12,17 @@ def makePairs(df):
     df = df.values.tolist()
     pairs = []
     for i in range(len(df)):
-        pairs.append((df[i], False))
+        pairs.append((df[i], None))
     return pairs
+
+def rangeQuery(df, point, radius):
+    new_df = []
+    for set in df:
+        new_df.append(set[0])
+    new_arrays = np.array(new_df)
+    tree = KDTree(new_arrays, leaf_size = 20)
+    indices = tree.query_radius(np.array(point), r = radius)
+    neighbors = []
+    for n in indices:
+        neighbors.append(new_arrays[n].tolist())
+    return neighbors
--- dbscan.py
+++ dbscan.py
@@ -1,6 +1,14 @@
 import aux as a
 
-def dbscan(df, r, density):
+def dbscan(df, r, minPoints):
     df = a.makePairs(df)
-    """ for point in df:
-        if  """
+    cluster = 0
+    for point in df:
+        if point[1] == None: #Without label
+            neighbors = a.rangeQuery(df, point[0], r)
+            if len(neighbors) < minPoints: #Atypical values
+                point[1] = False
+                continue
+            cluster += 1
+            point[1] = cluster
+            
